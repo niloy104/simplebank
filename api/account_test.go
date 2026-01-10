a79f9a09a02c47171874cd90240d46eab15f9f87
niloy104 <manzurul104@gmail.com>
2026-01-10 21:00:47 +0600

    write unit test for accounts api

---
+++ api/account_test.go
@@ -0,0 +1,50 @@
+package api
+
+import (
+	"fmt"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	mockdb "github.com/niloy104/simplebank/db/mock"
+	db "github.com/niloy104/simplebank/db/sqlc"
+	"github.com/niloy104/simplebank/util"
+	"github.com/stretchr/testify/require"
+	"go.uber.org/mock/gomock"
+)
+
+func TestGetAccountAPI(t *testing.T) {
+	account := randomAccount()
+
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	store := mockdb.NewMockStore(ctrl)
+	//build stubs
+	store.EXPECT().
+		GetAccount(gomock.Any(), gomock.Eq(account.ID)).
+		Times(1).
+		Return(account, nil)
+
+	//start test sever & send request
+	server := NewServer(store)
+	recorder := httptest.NewRecorder()
+
+	url := fmt.Sprintf("/accounts/%d", account.ID)
+	request, err := http.NewRequest(http.MethodGet, url, nil)
+	require.NoError(t, err)
+
+	server.router.ServeHTTP(recorder, request)
+
+	//check response
+	require.Equal(t, http.StatusOK, recorder.Code)
+}
+
+func randomAccount() db.Account {
+	return db.Account{
+		ID:       util.RandomInt(1, 1000),
+		Owner:    util.RandomOwner(),
+		Balance:  util.RandomMoney(),
+		Currency: util.RandomCurrency(),
+	}
+}
